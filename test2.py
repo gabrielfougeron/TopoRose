33cce754d620da2fea393c2ab55f76cf147bb644
Gabriel Fougeron <gabriel.fougeron@hotmail.fr>
2023-04-12 10:08:45 +0200

    updated tracks

--- test2.py
+++ test2.py
@@ -8,6 +8,7 @@ import geopandas as gpd
 
 import rasterio as rio
 import rasterio.plot
+import rasterio.mask
 
 import fiona
 
@@ -18,9 +19,26 @@ import shutil
 
 
 base_folder = os.path.dirname(__file__) 
-img_filename = os.path.join(base_folder,'geo_data/DEM/AP_02191_FBS_F0770_RT1/AP_02191_FBS_F0770_RT1.dem.tif')
+geo_data_folder = os.path.join(base_folder,'geo_data')
+img_filename = os.path.join(geo_data_folder,'DEM/AP_02191_FBS_F0770_RT1/AP_02191_FBS_F0770_RT1.dem.tif')
+
+model_bou_filename = os.path.join(geo_data_folder,'shp/model_bounds.shp')
+
+
+    
 
 with rio.open(img_filename) as img_open:
+
+    bou_outlines = gpd.read_file(model_bou_filename)
+    bou_outline_match = bou_outlines.to_crs(img_open.crs)
+
+    npoly = bou_outline_match['geometry'].shape[0]
+
+    MA,T,_ = rio.mask.raster_geometry_mask(img_open, bou_outline_match['geometry'], all_touched=False, invert=True)
+
+
+    print(MA)
+
 
     img = img_open.read()[0,...]
     #     
@@ -40,24 +58,24 @@ with rio.open(img_filename) as img_open:
     print('minval = ',minval)
     print('maxval = ',maxval)
 
-    xmin,xmax,ymin,ymax = rio.plot.plotting_extent(img_open)
-    
-    print('xmin = ',xmin)
-    print('xmax = ',xmax)
-    print('ymin = ',ymin)
-    print('ymax = ',ymax)
-
-    x = np.linspace(xmin, xmax, nx)
-    y = np.linspace(ymin, ymax, ny)
-
-    Y, X = np.meshgrid(y, x)
-
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-    # ax.contour3D(X, Y, img)
-    ax.plot_wireframe(X, Y, img)
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
+    # xmin,xmax,ymin,ymax = rio.plot.plotting_extent(img_open)
+    # 
+    # print('xmin = ',xmin)
+    # print('xmax = ',xmax)
+    # print('ymin = ',ymin)
+    # print('ymax = ',ymax)
+# 
+#     x = np.linspace(xmin, xmax, nx)
+#     y = np.linspace(ymin, ymax, ny)
+# 
+#     Y, X = np.meshgrid(y, x)
+# 
+#     fig = plt.figure()
+#     ax = plt.axes(projection='3d')
+#     # ax.contour3D(X, Y, img)
+#     ax.plot_wireframe(X, Y, img)
+#     ax.set_xlabel('x')
+#     ax.set_ylabel('y')
+#     ax.set_zlabel('z')
 
     plt.show()
